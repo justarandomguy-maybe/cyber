203c766da0c3533f4a23aa9da46f286b5b415026
player one <player@playground>
2025-01-25 11:53:28 -0500

    Add Keygen solver

---
+++ CTF/PICOCTF/ReverseEngineering/Medium/keygenme-trial_solver.py
@@ -0,0 +1,21 @@
+import hashlib
+from cryptography.fernet import Fernet
+import base64
+letters_order = [4,5,3,6,2,7,1,8]
+
+bUsername_trial = b"FRASER"
+
+hashed  =  hashlib.sha256(bUsername_trial).hexdigest()
+flag = ""
+for position in letters_order:
+    flag +=hashed[position]
+
+
+print(flag)
+
+
+key_part_static1_trial = "picoCTF{1n_7h3_|<3y_of_"
+key_part_dynamic1_trial = "ac73dc29"
+key_part_static2_trial = "}"
+key_full_template_trial = key_part_static1_trial + key_part_dynamic1_trial + key_part_static2_trial
+print(key_full_template_trial)
